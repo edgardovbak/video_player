7ab64d7ed2f31d9c3e4d6eea566893fe62ace609
dovbake <edhardowbak@gmail.com>
2018-11-06 15:14:47 +0100

    Create mpd files
    Convert video vith mp4box
    Update code

--- app/js/main.js
+++ app/js/main.js
@@ -1,108 +1,81 @@
-var mediaPlayer;
-var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-
-creating the MediaSource
-if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
-    var mediaSource = new MediaSource();
-    var url = URL.createObjectURL(mediaSource);
-    mediaSource.addEventListener('sourceopen', sourceOpen, false);
-} else {
-    console.error("mediasource or syntax not supported");
-}
-
-function sourceOpen (_) {
-    console.log(this.readyState);
-    var mediaSource = this;
-    var videoSourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-    initVideo();
+// helper function
+var log = function(msg) {
+  console.log(msg);
 };
 
-function initVideo () {
-    clearLog();
-    mediaPlayer = document.getElementById('media_video');
-    mediaPlayer.controls = false;
-    mediaPlayer.src = url;
-   fetch("https://html5-demos.appspot.com/static/test.webm").then(function(response) {
-      // The data has to be a JavaScript ArrayBuffer
-      return response.arrayBuffer();
-   }).then(function(videoData) {
-      videoSourceBuffer.appendBuffer(videoData);
-   });
+var FILE,CODEC,mediaSource,mediaPlayer;
+var NUM_CHUNKS = 6;
+
+document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
+
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('media_video');
+   mediaPlayer.controls = false;
+   window.MediaSource = window.MediaSource || window.WebKitMediaSource;
+   if (!!!window.MediaSource) {
+       alert('MediaSource API is not available');
+   }
+   start('mp4');
 }
 
-// Gets the mpd file and parses it
-function getData(url) {
-    if (url !== "") {
-        var xhr = new XMLHttpRequest(); // Set up xhr request
-        xhr.open("GET", url, true); // Open the request
-        xhr.responseType = "text"; // Set the type of response expected
-        xhr.send();
-
-        //  Asynchronously wait for the data to return
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == xhr.DONE) {
-                var tempoutput = xhr.response;
-                var parser = new DOMParser(); //  Create a parser object
-
-                // Create an xml document from the .mpd file for searching
-                var xmlData = parser.parseFromString(tempoutput, "text/xml", 0);
-                log("parsing mpd file");
-
-                // Get and display the parameters of the .mpd file
-                getFileType(xmlData);
-
-                // Set up video object, buffers, etc
-                setupVideo();
-
-                // Initialize a few variables on reload
-                clearVars();
-            }
+function callback() {
+    var sourceBuffer = mediaSource.addSourceBuffer(CODEC);
+    GET(FILE, function(uInt8Array) {
+        var file = new Blob([uInt8Array], {type: 'video/mp4'});
+        var chunkSize = Math.ceil(file.size / NUM_CHUNKS);
+        var i = 0;
+        (function readChunk_(i) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                sourceBuffer.appendBuffer(new Uint8Array(e.target.result));
+                console.log(e.target);
+                if (i == NUM_CHUNKS - 1) {
+                    mediaSource.endOfStream();
+                } else {
+                    if (mediaPlayer.paused) {
+                        mediaPlayer.play();
+                    }
+                    readChunk_(++i);
+                }
+            };
+            var startByte = chunkSize * i;
+            var chunk = file.slice(startByte, startByte + chunkSize);
+            reader.readAsArrayBuffer(chunk);
+        })(i);
+    });
+}
+function GET(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.send();
+    xhr.onload = function(e) {
+        if (xhr.status != 200) {
+            alert("Unexpected status code " + xhr.status + " for " + url);
+            return false;
         }
-
-        // Report errors if they happen during xhr
-        xhr.addEventListener("error", function (e) {
-            log("Error: " + e + " Could not load url.");
-        }, false);
+                console.log(xhr.response);
+        callback(new Uint8Array(xhr.response));
+    };
+}
+function start(type) {
+    if (type == 'webm') {
+        FILE = 'test.webm';
+        CODEC = 'video/webm; codecs="vorbis,vp8"';
     }
+    if (type == 'mp4') {
+        FILE = '/video/video_dashinit.mp4';
+        CODEC = 'video/mp4; codecs="avc1.64000d,mp4a.40.2"';
+    }
+    mediaSource = new MediaSource();
+    mediaPlayer.src = window.URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener('sourceopen', callback, false);
+    mediaSource.addEventListener('webkitsourceopen', callback, false);
+    mediaSource.addEventListener('webkitsourceended', function(e) {
+    }, false);
 }
 
-var mediaSource = new MediaSource();
-var url = URL.createObjectURL(mediaSource);
-var videoSourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-initVideo();
-// mediaSource.addEventListener('sourceopen', sourceOpen, false);
-
-
-// window.onload = function() {
-    var seekBar = document.getElementById("seek_bar");
-    //Event listener for the seek bar
-    seekBar.addEventListener("change", function() {
-        //Calculate new time
-        var newTime = mediaPlayer.duration * (seekBar.value / 100);
-        mediaPlayer.currentTime = newTime;
-    });
-
-    //As video progresses, seekBar moves forward
-    seekBar.addEventListener("timeupdate", function() {
-        var value = (100 / mediaPlayer.duration) * mediaPlayer.currentTime;
-        seekBar.value = value;
-    });
-
-    // Pause the video when the slider handle is being dragged
-    seekBar.addEventListener("mousedown", function() {
-        mediaPlayer.pause();
-    });
-
-    // Play the video when the slider handle is dropped
-    seekBar.addEventListener("mouseup", function() {
-        mediaPlayer.play();
-    });
-
-    var volumeBar = document.getElementById("volume_bar");
-    //Event listener for the volume slider
-    volumeBar.addEventListener("change", function() {
-        mediaPlayer.volume = volumeBar.value;
-    });
+window.onload = function() {
 
     var togglePlayPause = document.getElementById('play-pause-button');
     togglePlayPause.addEventListener("click", function() {
@@ -120,9 +93,40 @@ initVideo();
         }
     });
 
+    var seekBar = document.getElementById("seek-bar");
+    //Event listener for the seek bar
+    seekBar.addEventListener("change", function() {
+        //Calculate new time
+        var newTime = mediaPlayer.duration * (seekBar.value / 100);
+        mediaPlayer.currentTime = newTime;
+    });
+    console.log(mediaPlayer.duration );
+    //As video progresses, seekBar moves forward
+    seekBar.addEventListener("timeupdate", function() {
+        console.log(1231);
+        var value = (100 / mediaPlayer.duration) * mediaPlayer.currentTime;
+        seekBar.value = value;
+    }, false);
+
+    // Pause the video when the slider handle is being dragged
+    seekBar.addEventListener("mousedown", function() {
+        mediaPlayer.pause();
+    });
+
+    // Play the video when the slider handle is dropped
+    seekBar.addEventListener("mouseup", function() {
+        mediaPlayer.play();
+    });
+
+    var volumeBar = document.getElementById("volume-bar");
+    //Event listener for the volume slider
+    volumeBar.addEventListener("change", function() {
+        mediaPlayer.volume = volumeBar.value;
+    });
+
     var stopPlayer = document.getElementById('stop-button');
     stopPlayer.addEventListener("click", function() {
         mediaPlayer.pause();
         mediaPlayer.currentTime = 0;
     });
-// }
+}
--- build/js/main.min.js
+++ build/js/main.min.js
@@ -1,205 +1,81 @@
-var mediaPlayer;
-var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-
-// creating the MediaSource
-if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
-    var mediaSource = new MediaSource();
-    var url = URL.createObjectURL(mediaSource);
-    mediaSource.addEventListener('sourceopen', sourceOpen, false);
-} else {
-    console.error("mediasource or syntax not supported");
-}
-
-function sourceOpen (_) {
-    console.log(this.readyState);
-    clearLog();
-    mediaPlayer = document.getElementById('media_video');
-    var mediaSource = this;
-    mediaPlayer.src = url;
-    var videoSourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-    initVideo();
+// helper function
+var log = function(msg) {
+  console.log(msg);
 };
 
-function initVideo () {
+var FILE,CODEC,mediaSource,mediaPlayer;
+var NUM_CHUNKS = 6;
 
-    mediaPlayer.controls = false;
+document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
 
-   fetch("https://html5-demos.appspot.com/static/test.webm").then(function(response) {
-      // The data has to be a JavaScript ArrayBuffer
-      return response.arrayBuffer();
-   }).then(function(videoData) {
-      videoSourceBuffer.appendBuffer(videoData);
-   });
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('media_video');
+   mediaPlayer.controls = false;
+   window.MediaSource = window.MediaSource || window.WebKitMediaSource;
+   if (!!!window.MediaSource) {
+       alert('MediaSource API is not available');
+   }
+   start('mp4');
 }
 
-//  Load video's initialization segment
-function initVideo(range, url) {
-  var xhr = new XMLHttpRequest();
-  if (range || url) { // make sure we've got incoming params
-
-    // Set the desired range of bytes we want from the mp4 video file
-    xhr.open('GET', url);
-    xhr.setRequestHeader("Range", "bytes=" + range);
-    segCheck = (timeToDownload(range) * .8).toFixed(3); // use .8 as fudge factor
-    xhr.send();
+function callback() {
+    var sourceBuffer = mediaSource.addSourceBuffer(CODEC);
+    GET(FILE, function(uInt8Array) {
+        var file = new Blob([uInt8Array], {type: 'video/mp4'});
+        var chunkSize = Math.ceil(file.size / NUM_CHUNKS);
+        var i = 0;
+        (function readChunk_(i) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                sourceBuffer.appendBuffer(new Uint8Array(e.target.result));
+                console.log(e.target);
+                if (i == NUM_CHUNKS - 1) {
+                    mediaSource.endOfStream();
+                } else {
+                    if (mediaPlayer.paused) {
+                        mediaPlayer.play();
+                    }
+                    readChunk_(++i);
+                }
+            };
+            var startByte = chunkSize * i;
+            var chunk = file.slice(startByte, startByte + chunkSize);
+            reader.readAsArrayBuffer(chunk);
+        })(i);
+    });
+}
+function GET(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
     xhr.responseType = 'arraybuffer';
-    try {
-      xhr.addEventListener("readystatechange", function () {
-         if (xhr.readyState == xhr.DONE) { // wait for video to load
-          // Add response to buffer
-          try {
-            videoSource.appendBuffer(new Uint8Array(xhr.response));
-            // Wait for the update complete event before continuing
-            videoSource.addEventListener("update",updateFunct, false);
-
-          } catch (e) {
-            log('Exception while appending initialization content', e);
-          }
-        }
-      }, false);
-    } catch (e) {
-      log(e);
-    }
-  } else {
-    return // No value for range or url
-  }
-}
-
-//  Get video segments
-function fileChecks() {
-  // If we're ok on the buffer, then continue
-  if (bufferUpdated == true) {
-    if (index < segments.length) {
-      // Loads next segment when time is close to the end of the last loaded segment
-      if ((videoElement.currentTime - lastTime) >= segCheck) {
-        playSegment(segments[index].getAttribute("mediaRange").toString(), file);
-        lastTime = videoElement.currentTime;
-        curIndex.textContent = index + 1; // Display current index
-        index++;
-      }
-    } else {
-      videoElement.removeEventListener("timeupdate", fileChecks, false);
-    }
-  }
-}
-
-function timeToDownload(range) {
-  var vidDur = range.split("-");
-  // Time = size * 8 / bitrate
-  return (((vidDur[1] - vidDur[0]) * 8) / bandwidth)
-}
-
-function updateFunct() {
-  //  This is a one shot function, when init segment finishes loading,
-  //    update the buffer flag, call getStarted, and then remove this event.
-  bufferUpdated = true;
-  getStarted(file); // Get video playback started
-  //  Now that video has started, remove the event listener
-  videoSource.removeEventListener("update", updateFunct);
-}
-
-// Gets the mpd file and parses it
-function getData(url) {
-    if (url !== "") {
-        var xhr = new XMLHttpRequest(); // Set up xhr request
-        xhr.open("GET", url, true); // Open the request
-        xhr.responseType = "text"; // Set the type of response expected
-        xhr.send();
-
-        //  Asynchronously wait for the data to return
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == xhr.DONE) {
-                var tempoutput = xhr.response;
-                var parser = new DOMParser(); //  Create a parser object
-
-                // Create an xml document from the .mpd file for searching
-                var xmlData = parser.parseFromString(tempoutput, "text/xml", 0);
-                log("parsing mpd file");
-
-                // Get and display the parameters of the .mpd file
-                getFileType(xmlData);
-
-                // Set up video object, buffers, etc
-                setupVideo();
-
-                // Initialize a few variables on reload
-                clearVars();
-            }
-        }
-
-        // Report errors if they happen during xhr
-        xhr.addEventListener("error", function (e) {
-            log("Error: " + e + " Could not load url.");
-        }, false);
-    }
-}
-
-//  Play segment plays a byte range (format nnnn-nnnnn) of a media file
-function playSegment(range, url) {
-  var xhr = new XMLHttpRequest();
-  if (range || url) { // Make sure we've got incoming params
-    xhr.open('GET', url);
-    xhr.setRequestHeader("Range", "bytes=" + range);
     xhr.send();
-    xhr.responseType = 'arraybuffer';
-    try {
-      xhr.addEventListener("readystatechange", function () {
-        if (xhr.readyState == xhr.DONE) { //wait for video to load
-          //  Calculate when to get next segment based on time of current one
-            segCheck = (timeToDownload(range) * .8).toFixed(3); // Use .8 as fudge factor
-            segLength.textContent = segCheck;
-          // Add received content to the buffer
-          try {
-            videoSource.appendBuffer(new Uint8Array(xhr.response));
-          } catch (e) {
-            log('Exception while appending', e);
-          }
+    xhr.onload = function(e) {
+        if (xhr.status != 200) {
+            alert("Unexpected status code " + xhr.status + " for " + url);
+            return false;
         }
-      }, false);
-    } catch (e) {
-      log(e);
-      return // No value for range
+                console.log(xhr.response);
+        callback(new Uint8Array(xhr.response));
+    };
+}
+function start(type) {
+    if (type == 'webm') {
+        FILE = 'test.webm';
+        CODEC = 'video/webm; codecs="vorbis,vp8"';
     }
-  }
+    if (type == 'mp4') {
+        FILE = '/video/video_dashinit.mp4';
+        CODEC = 'video/mp4; codecs="avc1.64000d,mp4a.40.2"';
+    }
+    mediaSource = new MediaSource();
+    mediaPlayer.src = window.URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener('sourceopen', callback, false);
+    mediaSource.addEventListener('webkitsourceopen', callback, false);
+    mediaSource.addEventListener('webkitsourceended', function(e) {
+    }, false);
 }
 
-var mediaSource = new MediaSource();
-var url = URL.createObjectURL(mediaSource);
-var videoSourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-initVideo();
-// mediaSource.addEventListener('sourceopen', sourceOpen, false);
-
-
-// window.onload = function() {
-    var seekBar = document.getElementById("seek_bar");
-    //Event listener for the seek bar
-    seekBar.addEventListener("change", function() {
-        //Calculate new time
-        var newTime = mediaPlayer.duration * (seekBar.value / 100);
-        mediaPlayer.currentTime = newTime;
-    });
-
-    //As video progresses, seekBar moves forward
-    seekBar.addEventListener("timeupdate", function() {
-        var value = (100 / mediaPlayer.duration) * mediaPlayer.currentTime;
-        seekBar.value = value;
-    });
-
-    // Pause the video when the slider handle is being dragged
-    seekBar.addEventListener("mousedown", function() {
-        mediaPlayer.pause();
-    });
-
-    // Play the video when the slider handle is dropped
-    seekBar.addEventListener("mouseup", function() {
-        mediaPlayer.play();
-    });
-
-    var volumeBar = document.getElementById("volume_bar");
-    //Event listener for the volume slider
-    volumeBar.addEventListener("change", function() {
-        mediaPlayer.volume = volumeBar.value;
-    });
+window.onload = function() {
 
     var togglePlayPause = document.getElementById('play-pause-button');
     togglePlayPause.addEventListener("click", function() {
@@ -217,9 +93,40 @@ initVideo();
         }
     });
 
+    var seekBar = document.getElementById("seek-bar");
+    //Event listener for the seek bar
+    seekBar.addEventListener("change", function() {
+        //Calculate new time
+        var newTime = mediaPlayer.duration * (seekBar.value / 100);
+        mediaPlayer.currentTime = newTime;
+    });
+    console.log(mediaPlayer.duration );
+    //As video progresses, seekBar moves forward
+    seekBar.addEventListener("timeupdate", function() {
+        console.log(1231);
+        var value = (100 / mediaPlayer.duration) * mediaPlayer.currentTime;
+        seekBar.value = value;
+    }, false);
+
+    // Pause the video when the slider handle is being dragged
+    seekBar.addEventListener("mousedown", function() {
+        mediaPlayer.pause();
+    });
+
+    // Play the video when the slider handle is dropped
+    seekBar.addEventListener("mouseup", function() {
+        mediaPlayer.play();
+    });
+
+    var volumeBar = document.getElementById("volume-bar");
+    //Event listener for the volume slider
+    volumeBar.addEventListener("change", function() {
+        mediaPlayer.volume = volumeBar.value;
+    });
+
     var stopPlayer = document.getElementById('stop-button');
     stopPlayer.addEventListener("click", function() {
         mediaPlayer.pause();
         mediaPlayer.currentTime = 0;
     });
-// }
+}
